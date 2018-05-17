ba215f924c571f936f096ee77258c8bdb9c93ccb
moskowitza <moskowitzaaron@mac.com>
2018-05-17 11:05:53 -0400

    updated server with link

--- public/js/hoods.js
+++ public/js/hoods.js
@@ -39,8 +39,8 @@ $(document).ready(function() {
     newTr.data("hood", hoodData);
     newTr.append("<td>" + hoodData.name + "</td>");
     newTr.append("<td> " + hoodData.Posts.length + "</td>");
-    newTr.append("<td><a href='/blog?author_id=" + hoodData.id + "'>Go to Posts</a></td>");
-    newTr.append("<td><a href='/cms?author_id=" + hoodData.id + "'>Create a Post</a></td>");
+    newTr.append("<td><a href='/blog?hood_id=" + hoodData.id + "'>Go to Posts</a></td>");
+    newTr.append("<td><a href='/cms?hood_id=" + hoodData.id + "'>Create a Post</a></td>");
     newTr.append("<td><a style='cursor:pointer;color:red' class='delete-hood'>Delete Neighborhood</a></td>");
     return newTr;
   }
@@ -57,7 +57,7 @@ $(document).ready(function() {
     });
   }
 
-  // A function for rendering the list of authors to the page
+  // A function for rendering the list of Neighborhoods to the page
   function renderHoodList(rows) {
     hoodList.children().not(":last").remove();
     hoodContainer.children(".alert").remove();
