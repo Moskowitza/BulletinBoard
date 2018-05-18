45ffd662462a3f38fcc8159f57e885486cebbb62
moskowitza <moskowitzaaron@mac.com>
2018-05-17 20:02:02 -0400

    fixing routes

--- controller/controller.js
+++ controller/controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (post.js) to use its database functions.
-var post = require("../models/post");
+var post = require("../models/post.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
