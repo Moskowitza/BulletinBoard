4bf39e34a4661305855191f83e5e647d18b9d687
moskowitza <moskowitzaaron@mac.com>
2018-05-21 14:17:01 -0400

    added helpers for handlebars

--- public/js/blog.js
+++ public/js/blog.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
         console.log("posted?")
       });
   });
-
+// UP VOTE BLOCK
   $(".upVote").on("click", function (event) {
     event.preventDefault();
     var id=$(this).data("id");
@@ -29,6 +29,27 @@ $(document).ready(function () {
     var newRankObj = {
       id : id,
       rank : rank + 1
+    };
+    console.log("New Rank : "+ newRankObj)
+    $.ajax("/api/vote"+ id,{
+      type:"PUT",
+      data: newRankObj})
+    .then(function () {
+      console.log("upvoted");
+      location.reload();
+    });
+  });
+// DOWN VOTE BLOCK
+
+  $(".downVote").on("click", function (event) {
+    event.preventDefault();
+    var id=$(this).data("id");
+    var rank=$(this).data("rank");
+    console.log("current Rank: "+ rank)
+    var newRank= rank - 1;
+    var newRankObj = {
+      id : id,
+      rank : rank -1
     };
     console.log("New Rank : "+ newRankObj)
     $.ajax("/api/vote"+ id,{
