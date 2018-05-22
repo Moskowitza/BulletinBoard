5d810a7ab862e88f0812bb6600613418cbdf9526
Nora Granahan <namgranahan@gmail.com>
2018-05-21 20:42:32 -0400

    fixed lat lng to center hood map

--- controller/controller.js
+++ controller/controller.js
@@ -63,9 +63,9 @@ router.get("/hoods/:id", function (req, res) {
     var hbsPosts = {
       posts: data
     }
-    var hoodName = hbsPosts.posts[0].Hood.name;
+    // var hoodName = hbsPosts.posts[0].Hood.name;
     console.log("SELECTED hbsHood is: " + JSON.stringify(hbsPosts));
-    console.log("hood name: " + hoodName)
+    // console.log("hood name: " + hoodName)
     res.render("hoods", hbsPosts);
 
     // console.log("SELECTED hbsHood is: " + hbsHood);
