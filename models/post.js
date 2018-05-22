11c4bc853a809425e94973b633dc56cef5ae368a
moskowitza <moskowitzaaron@mac.com>
2018-05-22 18:15:43 -0400

    set default rank

--- models/post.js
+++ models/post.js
@@ -14,6 +14,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     rank: {
       type: DataTypes.INTEGER,
+      defaultValue: 1,
       allowNull: false,
       len: [1]
     }
--- public/js/blog.js
+++ public/js/blog.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     var newPost = {
       title: $("#title").val().trim(),
       body: $("#body").val().trim(),
-      rank: $("#rank").val().trim(),
+      // rank: $("#rank").val().trim(),
       // $("#neighborhood").val() works!!!! but still need to write to database
       HoodID: $("#neighborhood").val()
     };
