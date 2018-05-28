1afc77d34a92fa01792305fe39ae38f53ba504d7
moskowitza <moskowitzaaron@mac.com>
2018-05-28 15:50:57 -0400

    nightmare script

--- tests/userTest.js
+++ tests/userTest.js
@@ -3,17 +3,25 @@ var Nightmare = require("nightmare");
 var nightmare = Nightmare({ show: true });
 
 nightmare
-  .goto("https://stormy-temple-91988.herokuapp.com/")
-  .type("#search_form_input_homepage", "github nightmare")
-  .click("#search_button_homepage")
-  .wait("#links a")
-  .evaluate(function() {
-    return document.querySelector("#links a").href;
-  })
-  .end()
-  .then(function(result) {
-    console.log(result);
-  })
-  .catch(function(error) {
-    console.error("Search failed:", error);
-  });
+    // go to home page
+    .goto("https://stormy-temple-91988.herokuapp.com/")
+    //   go to add new post
+    .goto("https://stormy-temple-91988.herokuapp.com/newpost")
+
+    .type("#title", "Best Pizza")
+    .type("#body", "Went to Chicks and had the best Pizza last night")
+    .type("#location", "1807 Washington Ave, Philadelphia, PA 19146")
+    // Pick neighborhood using value 
+    .select("#neighborhood", [value=3])
+    //submit
+    .click(".submitButton")
+    //wait for the alert
+    .wait(3000)
+    // Nightmare disables window.alert from popping up by default, but you can still listen for the contents of the alert dialog.
+    .end()
+    .then(function (result) {
+        console.log(result);
+    })
+    .catch(function (error) {
+        console.error("NewPost failed:", error);
+    });
